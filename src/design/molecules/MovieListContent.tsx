1bf1cf5277292666a116d016b766a865e825349a
rayane.chilali <rayane.chilali@thalesgroup.com>
2023-11-20 22:24:27 +0100

    feature: add movie list page searching

--- src/design/molecules/MovieListContent.tsx
+++ src/design/molecules/MovieListContent.tsx
@@ -3,17 +3,22 @@ import {MovieList} from "../atoms/MovieList.tsx";
 import {Link} from "react-router-dom";
 import {PosterImage} from "../atoms/images/PosterImage.tsx";
 
-export const MovieListContent = () => {
+interface MovieListContentProps {
+  searchTerm: string;
+}
 
-    const {isLoading, error, data: movies} = useFetchNowPlayingMovies();
+export const MovieListContent = ({searchTerm}: MovieListContentProps) => {
+
+  const {isLoading, error, data: movies} = useFetchNowPlayingMovies();
 
   if (error) return <div>Request Failed</div>;
   if (isLoading) return <div>Loading...</div>;
 
+  const filteredMovies = movies?.filter(movie => movie.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
   return (
       <MovieList>
-        {movies?.map(({poster_path, id}) => (
+        {filteredMovies?.map(({poster_path, id}) => (
             <Link key={id} to={"/movie/" + id}>
               <PosterImage key={id} src={"https://www.themoviedb.org/t/p/w600_and_h900_bestv2" + poster_path} alt="alt"></PosterImage>
             </Link>
