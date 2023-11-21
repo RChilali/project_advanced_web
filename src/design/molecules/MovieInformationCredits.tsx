64059783193794044b0547f9691890c2dccd31fa
daria <54882674+dkulieshova@users.noreply.github.com>
2023-11-21 14:26:54 +0100

    feature: add images on load

--- src/design/molecules/MovieInformationCredits.tsx
+++ src/design/molecules/MovieInformationCredits.tsx
@@ -10,15 +10,17 @@ interface MovieInformationCreditsProps {
   profile_path: string;
 }
 
-export const MovieInformationCredits = ({
-                                          name,
-                                          character,
-                                          profile_path
-                                        }: MovieInformationCreditsProps) => {
-  return (
+export const MovieInformationCredits = ({name, character, profile_path }: MovieInformationCreditsProps) => {
+    if (profile_path == undefined) {
+        profile_path = "/no_photo.png"
+    }
+    else
+        profile_path = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2" + profile_path
+
+    return (
       <CreditsHolder>
         <CreditsImage
-            src={"https://www.themoviedb.org/t/p/w600_and_h900_bestv2" + profile_path}/>
+            src={profile_path}/>
         <Text textColor={"white"} variant={TextVariant.small} isItalic={false} isMediaActive={false}>{name}</Text>
         <Text textColor={"#D6D3D1"} variant={TextVariant.small} isItalic={false} isMediaActive={false}>{character}</Text>
       </CreditsHolder>
